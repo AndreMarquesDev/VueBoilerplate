7d3035cf80979ecb453e3bbe7712a82e4eb2f644
Andre.Marques <andre.marques@thegrandunion.com>
2019-04-08 16:18:08 +0100

    UPD: Added transition behavior

--- sources/app.vue
+++ sources/app.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <transition name="fadeTransition" mode="out-in">
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 
